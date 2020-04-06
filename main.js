5136fd01aca1897bf5d8abbbee711d0d4037b3fd
Clarke Newsham <clarke.newsham92@gmail.com>
2020-04-06 11:44:26 +0100

    Degree and module http request json data functions, Render HTML Degree & Module data functions implemented

--- main.js
+++ main.js
@@ -7,24 +7,88 @@ var assessmentPage = document.getElementById("assessment-page");
 
 // HTML button links
 var dashboardButton = document.getElementById("dashboard-button");
-var degreesButton = document.getElementById("degrees-button");
+var degreesButton = document.getElementById("degree-button");
 var moduleButton = document.getElementById("module-button");
 var assessmentButton = document.getElementById("assessment-button");
 
+// Container to hold JSON Data
+var degreeContainer = document.getElementById('degree-container');
+var moduleContainer = document.getElementById('module-container');
+var assessmentContainer = document.getElementById('assessment-container');
 
-// hide dashbaord and reveal degree path page
+// hide dashbaord and reveal degree path page with degree object json data
 degreesButton.addEventListener("click", function(){
   degreePage.hidden = false;
   dashboard.hidden = true;
-  
+  var degreeRequest = new XMLHttpRequest();
+  degreeRequest.open('GET', 'https://raw.githubusercontent.com/clarkenewsh/Academic-Tack-WebApp/master/degree.json');
+  degreeRequest.onload = function() {
+    console.log(degreeRequest.responseText);
+    var degreeData = JSON.parse(degreeRequest.responseText);
+    showDegreesSection(degreeData);
+  };
+  degreeRequest.send();
 });
+
+
+
+//Render HTML Degrees JSON data
+function showDegreesSection(data){
+  var htmlString = "";
+   for(var i = 0; i < data.length; i++) {
+    htmlString += '<div class="container">' + 
+                      '<div class="card col">' +
+                        '<div class="card-body">' +
+                          '<h4>' + data[i].id + '</h4>' +
+                          '<h5>' + data[i].name + '</h5>' +
+                        '</div>' +
+                      '</div>' +
+                    '</div>';
+
+
+  }
+  degreeContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+
+
 
 // hide dashbaord and reveal modules page
 moduleButton.addEventListener("click", function(){
   dashboard.hidden = true;
   assessmentPage.hidden = true;
   modulePage.hidden = false;
+
+  // GET request 
+  var moduleRequest = new XMLHttpRequest();
+  moduleRequest.open('GET', 'https://raw.githubusercontent.com/clarkenewsh/Academic-Tack-WebApp/master/module.json');
+  moduleRequest.onload = function() {
+    console.log(moduleRequest.responseText);
+    var moduleData = JSON.parse(moduleRequest.responseText);
+    showModuleSection(moduleData);
+  };
+  moduleRequest.send();
+
 });
+
+//Render HTML Modules JSON data
+function showModuleSection(mdata){
+  var htmlString = "";
+   for(var i = 0; i < data.length; i++) {
+    htmlString += '<div class="container">' + 
+                      '<div class="card col">' +
+                        '<div class="card-body">' +
+                          '<h4>' + mdata[i].id + '</h4>' +
+                          '<h5>' + mdata[i].title + '</h5>' +
+                          '<p>' + mdata[i].hours + '</p>' +
+                        '</div>' +
+                      '</div>' +
+                    '</div>';
+
+
+  }
+  moduleContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+
 
 // hide dashbaord and reveal assessments page
 assessmentButton.addEventListener("click", function(){
@@ -32,17 +96,15 @@ assessmentButton.addEventListener("click", function(){
   modulePage.hidden = true;
   degreePage.hidden = true;
   assessmentPage.hidden = false;
-  
 });
 
-
+// Return to dashboard functionality - reload content on refresh
 dashboardButton.addEventListener("click", function(){
   dashboard.hidden = false;
   degreePage.hidden = true;
   modulePage.hidden = true;
   assessmentPage.hidden = true;
- 
-
+  window.location.reload();
 });
 
 
